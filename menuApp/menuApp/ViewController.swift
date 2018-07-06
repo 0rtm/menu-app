a02455d600d6e7318a5e799c10376be9803bd7f7
Artem Tselikov <ar.tselikov@gmail.com>
2018-07-06 18:40:11 -0400

    add empty project

---
+++ menuApp/menuApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  menuApp
+//
+//  Created by Artem Tselikov on 2018-07-06.
+//  Copyright © 2018 Artem Tselikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
